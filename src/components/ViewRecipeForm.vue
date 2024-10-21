ab9e0e04ea8c9e19b35bf8ef1d7a9a7c161a1ac7
chromody <chromody@gmail.com>
2024-10-20 23:19:07 -0500

    updated ingredients view

--- src/components/ViewRecipeForm.vue
+++ src/components/ViewRecipeForm.vue
@@ -13,9 +13,18 @@
           </div>
 
           <div class="modal-body">
-            <form @submit.prevent="viewRecipe">
-              <div class="ingredients"> 
+            <form @submit.prevent="getRecipe">
+              
+              <div class="ingredients">
+                <h5>Ingredients:</h5>
+                <ul>
+                  <li v-for="ingredient in ingredients" :key="ingredient.id">
+                    {{ ingredient.name }} ({{ ingredient.quantity }} {{ ingredient.minQuantity }} {{ ingredient.unit }})
+                  </li>
+                </ul>
               </div>
+
+
               <button id="submitRecipe" type="submit" class="btn btn-primary w-100" data-bs-dismiss="">Edit</button>
             </form>
           </div>
@@ -51,62 +60,44 @@ export default {
       type: Object,
       default: () => ({
           id: '0',
-          name: 'NULL',
-          description: "NULL",
-          rating: 0,
-          prepTime: 0,
-          prepUnit: "NULL",
-          cookTime: 0,
-          cookUnit: "NULL",
         }),
+    ingredient: {
+      type: Object,
+      default: () => ({
+        //{{ ingredient.name }} ({{ ingredient.quantity }} {{ ingredient.minQuantity }} {{ ingredient.unit }})
+        id: '0',
+        name: '',
+        quantity: 0,
+        minQuantity: 0,
+        unit: '',
+      }),
+    }
     }
   },
   data() {
     return {
       id: '0',
-      name: '',
-      description: "",
-      rating: 0,
-      prepTime: 0,
-      prepUnit: "",
-      cookTime: 0,
-      cookUnit: "",
+      ingredients: [], // New data property for ingredients
     };
   },
   created() {
     this.populateForm();
+    this.getRecipe();
   },
   watch: {
     recipe: {
       immediate: true,
       handler() {
         this.populateForm();
+        this.getRecipe();
       }
     }
   },
   methods: {
     populateForm() {
       this.id = this.recipe.id;
-      this.name = this.recipe.name;
-      this.description = this.recipe.description;
-      this.rating = this.recipe.rating;
-      this.prepTime = this.recipe.prepTime;
-      this.prepUnit = this.recipe.prepUnit;
-      this.cookTime = this.recipe.cookTime;
-      this.cookUnit = this.recipe.cookUnit;
     },
-    async viewRecipe() {
-      var input = {
-        id: this.id,
-        name: this.name,
-        description: this.description,
-        rating: this.rating,
-        prepTime: this.prepTime,
-        prepUnit: this.prepUnit,
-        cookTime: this.cookTime,
-        cookUnit: this.cookUnit,
-      }
-
+    async getRecipe() {
       const headers = {
           'Content-Type': 'application/json', // Specify content type
           'Authorization': `${sessionStorage.getItem('SessionId')}`, // Add an authorization token
@@ -123,22 +114,8 @@ export default {
           Unit = init.Unit,
           Notes = init.Notes
         */
-        const response = await axios.put('https://bakery.permavite.com/api/recipes/', input, { headers });
-
-
-        this.$emit('close-modal');
-        this.name = '';
-        this.description = '';
-        this.rating = 0;
-        this.prepTime = 0;
-        this.prepUnit = '';
-        this.cookTime = 0;
-        this.cookUnit = '';
-        Swal.fire({
-          title: "Recipe Changed!",
-          text: "",
-          icon: "success"
-        });
+        const response = await axios.put('https://bakery.permavite.com/api/ingredients/recipeid/' + this.id, input, { headers });
+        this.ingredients = response.data;
       } catch (e) {
         Swal.fire({
           title: "Error!",
